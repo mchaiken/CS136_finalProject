22f68412e9a859be0857e8fabde1baa21a57c632
Miranda Chaiken <mirandachaiken@aol.com>
2015-12-02 21:22:59 -0500

    Python graph

---
+++ GraphList.py
@@ -0,0 +1,75 @@
+class Edge:
+    
+    def __init__(self, start, end, weight= None):
+        self._start = start
+        self._end = end
+        self._weight = weight
+        self._visited = False
+    
+    def visit(self):
+        self._visited = True
+
+    def visited(self):
+        return self._visited
+
+    def getEnd(self):
+        return self._end
+
+    def getStart(self):
+        return self._start
+
+
+class GraphList:
+    class _Node:
+        def __init__(self, label):
+            self.label = label
+            self._edgeList = []
+            self._dist = sys.maxint
+            self._visited = False
+        
+        def label(self):
+            return self.label
+        
+        def visit(self):
+            self._visited = True
+        
+        def visited(self):
+            return self._visited
+        
+        def addEdge(self, e):
+            self._edgeList.append(e)
+    
+    def __init__():
+        self.nodeDict = {}
+    
+    # Mark vertex with label label as “visited” and return previous value of visited flag
+    def visit(self, label):
+        self._nodeDict[label].vist()
+    
+    
+    # Returns true iff vertex/edge has been visited
+    def visited(self, label):
+        return self._nodeDict[label].visted()
+    
+    
+    #View of all vertices adjacent to vertex with label label
+    def getEdgeList(self, label):
+        return self._nodeDict[label]._edgeList
+
+    #View of all vertices in graph
+    def vertices(self):
+        pass
+ 
+    def add(self, label):
+        node = GraphList._Node(label)
+        self.nodeDict[label] = node
+    
+    def addEdge(self, here, there, edge_weight=None):
+        nodeHere = self.nodeDict[here]
+        nodeThere = self.nodeDict[there]
+        nodeHere.addEdge( Edge(here, there, edge_weight) )
+        nodeThere.addEdge( Edge(there, here, edge_weight) )
+    
+    # Remove visit
+    def reset(self):
+        pass
