0d682c31a99226059d3402c8c7700e7fc8b01593
Nick Post <nbp1@williams.edu>
2015-12-05 14:20:04 -0500

    Add testmain

---
+++ TestMain.py
@@ -0,0 +1,27 @@
+# Testing main script. Loads test xml data into
+# a graph and tests our algorithms/data structures.
+
+import xml.etree.ElementTree as et
+from GraphList import *
+import sys
+
+def main(dataFileName, interactive=False):
+    graph = GraphList()
+    loadData(dataFileName, graph)
+    if (interactive):
+        return graph
+
+def loadData(fileName, graph):
+    tree = et.parse(fileName)
+    root = tree.getroot()
+    for elem in root.iter("*"):
+        if elem.tag == "node":
+            # Add node
+            graph.addNode(int(elem.get("id")))
+        elif elem.tag == "edge":
+            # Add edge
+            graph.addEdge(int(elem.get("here")), int(elem.get("there")), 
+                          float(elem.get("weight")))
+
+if __name__ == '__main__':
+    main(sys.argv[1])
